57932d8dc49f7a2a42d61742fa8959a28abc589b
samira kibous <kiboussamira71@gmail.com>
2025-09-30 16:00:41 +0100

    fixing bug

--- server.js
+++ server.js
@@ -7,3 +7,12 @@ const adminRoutes = require("./src/routes/admin.routes");
 app.use("/admin", adminRoutes);
 
 app.listen(3000, () => console.log("Serveur démarré sur le port 3000"));
+
+
+// const app = require('./src/app');
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//     console.log(`SERVER RUNNING ON PORT ${PORT}`)
+// })
