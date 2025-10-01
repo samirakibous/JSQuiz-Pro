8c81b3cb67329276e460fb823bfbc85c770403f2
samira kibous <kiboussamira71@gmail.com>
2025-10-01 11:19:51 +0100

    fixing middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,8 @@
 const {verifyToken} = require('../utils/jwt');
 
 const authenticateToken = (req, res, next) => {
-    const token = req.session?.token || req.headers['authorization']?.split(''[1]);
+    // const token = req.session?.token || req.headers['authorization']?.split(''[1]);
+    const token = req.session?.token || req.headers['authorization']?.split(' ')[1];
 
     if (!token) {
          return res.status(401).redirect('/login');
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,9 +1,11 @@
 const express= require("express");
 const router =express.Router();
-const isAuthenticated = require('../middlewares/auth.middleware');
-const authorizeRoles = require("../middlewares/role.middleware");
+// const { authenticateToken } = require('../middlewares/auth.middleware');
+// const authorizeRoles = require("../middlewares/role.middleware");
+const { authenticateToken } = require('../middlewares/auth.middleware');
+const authorizeRoles = require('../middlewares/role.middleware');
 const adminController = require("../controllers/admin.controller");
 
-router.post('/questions', adminController.createQuestion);
-router.put('/questions/:id', adminController.updateQuestion);
+router.post('/questions', authenticateToken,authorizeRoles('admin'), adminController.createQuestion);
+router.put('/questions/:id', authenticateToken,authorizeRoles('admin'), adminController.updateQuestion);
 module.exports = router;
